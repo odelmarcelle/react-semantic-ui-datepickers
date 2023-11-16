55d16df961fbb55c5c35b5c4f0f5d1d9979cc50b
odlmarce <delmarcelle.olivier@gmail.com>
2023-11-16 16:56:55 +0100

    commit dist

---
+++ dist/components/input.d.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Input, FormInputProps, FormFieldProps } from 'semantic-ui-react';
+import { SemanticDatepickerProps } from '../types';
+declare type InputProps = FormInputProps & {
+    clearIcon: SemanticDatepickerProps['clearIcon'];
+    icon: SemanticDatepickerProps['icon'];
+    isClearIconVisible: boolean;
+    fieldProps: FormFieldProps;
+    fieldRef: React.Ref<HTMLElement>;
+    children?: React.ReactNode;
+};
+declare const CustomInput: React.ForwardRefExoticComponent<Pick<InputProps, keyof FormInputProps> & React.RefAttributes<Input>>;
+export default CustomInput;
